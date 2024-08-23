d42e3abf593ac2dcb93145381c16bf0c5a24ad20
Anton Zelenov <antonz@rolos.com>
2024-08-23 14:05:08 +0200

    Store app instance to prevent killing

--- app/main.py
+++ app/main.py
@@ -3,12 +3,16 @@ import sys
 from PyCommonist import PyCommonist
 from PyQt6.QtWidgets import QApplication
 
+_application_storage: set[PyCommonist] = set()
+
 
 def main():
+    global _application_storage
     app = QApplication(sys.argv)
     # No logo for now, stay tuned
     # app.setWindowIcon(QIcon("img/Logo PyCommonist.svg"))
-    PyCommonist()
+    # This variable should be alive, otherwise it will be destroyed by GC
+    _application_storage.add(PyCommonist())
     sys.exit(app.exec())
 
 
